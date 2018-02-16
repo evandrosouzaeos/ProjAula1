07139258aa4c2db7139a3481f94a4a0cde136080
Aluno Fiar Logatti <aluno@fiar.local>
2018-02-16 21:52:40 -0200

    implementacao

---
+++ ProjAula1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjAula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            int r = 0;
+
+            //imprimir
+            Console.WriteLine("Teste aplicação!");
+
+            //popular variáveis
+            Console.WriteLine("Informe o valor de a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor de a: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            //operação: somar
+            r = a + b;
+
+            if (r >= 9)
+            {
+                Console.WriteLine("Que soma!!! Tá voando");
+            }
+            else
+            {
+                Console.WriteLine("Tente outra vez!");
+            }
+
+            Console.WriteLine("A soma dos valores informados é: " + r);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProjetoAula1-Exemplo2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAula1_Exemplo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int nota = 0;
+            int aux = 0;
+
+            Console.Write("Nota:");
+
+            while (aux == 0)
+            {
+                try
+                {
+                    nota = Convert.ToInt32(Console.ReadLine());
+                    aux = 1;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Caracter inválido!");
+                    Console.Write("Notassss:");
+                }
+            }
+
+            while (nota != 0) 
+            {
+
+                aux = 0;
+
+                switch (nota)
+                {
+                    case 1:
+
+                        Console.WriteLine("A nota é 1");
+                        break;
+
+                    case 5:
+
+                        Console.WriteLine("A nota é 5");
+                        break;
+
+                    default:
+                        Console.WriteLine("Sabe lá... que nota é essa!");
+                        break;
+                }
+                Console.Write("Nota:");
+                while (aux == 0)
+                {
+                    try
+                    {
+                        nota = Convert.ToInt32(Console.ReadLine());
+                        aux = 1;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Caracter inválido!");
+                        Console.Write("Nota:");
+                    }
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
